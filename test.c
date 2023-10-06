cbbfa7ce5bff13824ef78e68e4e8db903c44d1d3
alanfvn <alandavidg16@gmail.com>
2023-10-05 22:10:50 -0600

    xd

--- test.c
+++ test.c
@@ -1,17 +1,11 @@
 #include <cstdlib>
 #include <stdio.h>
-#include <time.h>
 #include <cstring>
+#include <time.h>
 #include "funcs.h"
 
-//type definitions
+//TYPE DEFS
 typedef int (* FuncPtr)();
-
-typedef struct {
-  double execute_time;
-  char** output_list;
-  int exit_code;
-} TestResult;
 
 // FUNCIONES DE AYUDA
 int isPrime(int num);
@@ -24,54 +18,32 @@ int checkPrimeNumberSet();
 int checkLargePrimeNumber();
 int checkBaseCaseNum1();
 
-
-TestResult executeTests(){
-
+int executeTests(){
   FuncPtr pruebas[] = {checkBaseCaseNum1, checkPrimeNumberSet, checkCompositeNumberSet, checkNegativeNumberSet, checkLargePrimeNumber};
-  const char* nombres[] = {
-    "Comprobar caso numero 1", "Comprobar set numeros primos",
-    "Comprobar set numeros compuestos", "Comprobar de numeros negativos",
-    "Comprobar numero primo grande", 
-  };
+  const char* nombres[] = {"Comprobar caso numero 1", "Comprobar set numeros primos", "Comprobar set numeros compuestos", "Comprobar de numeros negativos", "Comprobar numero primo grande", };
   int testSize = sizeof(pruebas) / sizeof(pruebas[0]);
   // TEST RESULT VARIABLES
-  TestResult result;
   clock_t start_time, end_time;
-  double exec_time;
+  double test_time;
   //START TRACKING
   start_time = clock();
-  result.output_list = (char**)malloc(testSize * sizeof(char*));
-  for (int i = 0; i < testSize; i++) {
-    result.output_list[i] = NULL; // Inicializar a NULL
-    // Resto del código...
-  }
 
   for (int i = 0; i < testSize; i++) {
     int resultado = pruebas[i]();
     char* resultString = getTestOutput(nombres[i], resultado);
-    // Asignar memoria para la string y copiar el contenido
-    result.output_list[i] = (char*)malloc((strlen(resultString) + 1) * sizeof(char));
-    strcpy(result.output_list[i], resultString);
-    // Liberar la memoria de resultString ya que no se necesita más
+    printf("%s", resultString);
     free(resultString);
 
     if(resultado == EXIT_FAILURE){
-      //CALCULATE TEST DURATION
       end_time = clock();
-      exec_time = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
-      result.execute_time = exec_time;
-      result.exit_code = EXIT_FAILURE;
-      return result;
+      test_time = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
+      return EXIT_FAILURE;
     }
   }
-
   //CALCULATE TEST DURATION
   end_time = clock();
-  exec_time = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
-  result.execute_time = exec_time;
-  result.exit_code = EXIT_SUCCESS;
-
-  return result;
+  test_time =((double)(end_time - start_time)) / CLOCKS_PER_SEC; 
+  return EXIT_SUCCESS;
 }
 
 // AREA DE PRUEBAS
@@ -109,7 +81,7 @@ int checkNegativeNumberSet(){
 }
 
 int checkLargePrimeNumber(){
-  // int largePrime = 2147483647;
-  int largePrime = 46273;
+  int largePrime = 2147483647;
+  // int largePrime = 46273;
   return isPrime(largePrime) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
--- test.h
+++ test.h
@@ -1,12 +1,1 @@
-// test_result.h
-#ifndef TEST_RESULT_H
-#define TEST_RESULT_H
-
-typedef struct {
-  double execute_time;
-  char** output_list;
-  int exit_code;
-} TestResult;
-
-#endif // TEST_RESULT_H
-TestResult executeTests();
+int executeTests();
