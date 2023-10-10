6aacf1d3d8eba442a7176732bec2939b97109d7a
miguemi <manuelmiguel80@hotmail.com>
2023-10-10 01:11:30 -0600

    :clown_face: se modificó archivo util

--- src/utils.c
+++ src/utils.c
@@ -10,13 +10,7 @@ int isPrime(int number) {
   if (number < 2) { 
     return false; 
   }
-  for (int i = 2; i * i <= number; i++) {
-    if (number % i == 0) {
-      return false;
-    }
-  }
-  return true;
-}
+
 
 int checkArgs(int argc, char *argv[]){
   return argc > 1 && strcmp(argv[1], "-test") == 0;
