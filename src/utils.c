59d96a7f29d8816a93206352969937561a306caa
miguemi <manuelmiguel80@hotmail.com>
2023-10-10 19:59:56 -0600

    :clown_face: se eliminó if en archivo util

--- src/utils.c
+++ src/utils.c
@@ -7,9 +7,7 @@
 #define COLOR_RESET   "\x1b[0m"
 
 int isPrime(int number) {
-  if (number < 2) { 
-    return false; 
-  }
+ 
   for (int i = 2; i * i <= number; i++) {
     if (number % i == 0) {
       return false;
